ac363ca177a4ccb2c8e2eb9eabba8525d4bff72f
kayubakar0 <suryaadrian970@gmail.com>
2021-01-04 08:29:53 +0700

    update

--- responsi/index.php
+++ responsi/index.php
@@ -24,7 +24,7 @@ function display_login_form(){ ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login - Catatan Harianku</title>
+    <title>Login</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <style>
         body{
